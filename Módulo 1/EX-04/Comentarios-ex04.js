e00afa9ef3e093d2f150b5a5da109dde86e29217
Jefferson Barbosa <119863688+Jefferson-BR@users.noreply.github.com>
2023-12-06 19:17:52 -0300

    Add files via upload

---
+++ Módulo 1/EX-04/Comentarios-ex04.js
@@ -0,0 +1,38 @@
+/*
+
+- Operadores aritméticos:
+
++ (Adição)
+- (Subtração)
+* (multiplicação)
+/ (Divisão)
+% (Resto da Divisão)
+** (Potência ou elevado ao quadrado)
+
+- Auto-Atribuição: 
+
+var numero = 3
+
+numero = numero + 4
+numero = numero - 5
+numero = numero * 4
+numero = numero / 2
+numero = numero ** 2
+numero = numero % 5
+
+
+(A Syntxe abaixo só funciona se a variavel receber ela mesma)
+
+numero += 4
+numero -= 5
+numero *= 4
+numero /= 2
+numero **= 2
+numero %= 5
+
+- Incremento:
+
+numero ++
+numero --
+
+*/
