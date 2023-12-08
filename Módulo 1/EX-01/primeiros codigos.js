bf77d21d5755f36a569e1d87e03bf15d39a1364e
Jefferson Barbosa <119863688+Jefferson-BR@users.noreply.github.com>
2023-12-08 10:27:35 -0300

    Delete Módulo 1 directory

--- Módulo 1/EX-01/primeiros codigos.js
+++
@@ -1,3 +0,0 @@
-alert("Essa é uma caixa de alerta.")
-confirm("Essa é uma caixa de confirmação! Clique em qualquer botão para continuar...")
-prompt("Essa é uma caixa de interação com o Usuário, Digite qualquer coisa e depois clique em um dos botões.")
